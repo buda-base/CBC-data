b80fda35678e39d8e989f50c323480dc986a4145
eroux <elie.roux@telecom-bretagne.eu>
2020-10-22 16:31:52 +0200

    add seeAlso to outline nodes
    for https://github.com/buda-base/public-digital-library/issues/288

--- create-lod.py
+++ create-lod.py
@@ -262,6 +262,8 @@ for T in ALL_T:
         #LOD_G.add((expr, ADM.seeOtherCBETA, Literal("http://cbetaonline.dila.edu.tw/%s" % T, datatype=XSD.anyURI)))
         LOD_G.add((expr, RDFS.seeAlso, Literal("http://21dzk.l.u-tokyo.ac.jp/SAT2018/%s.html" % TforSAT, datatype=XSD.anyURI)))
         LOD_G.add((expr, RDFS.seeAlso, Literal("http://cbetaonline.dila.edu.tw/%s" % T, datatype=XSD.anyURI)))
+        LOD_G.add((res, RDFS.seeAlso, Literal("http://21dzk.l.u-tokyo.ac.jp/SAT2018/%s.html" % TforSAT, datatype=XSD.anyURI)))
+        LOD_G.add((res, RDFS.seeAlso, Literal("http://cbetaonline.dila.edu.tw/%s" % T, datatype=XSD.anyURI)))
     if T in T_TO_CN:
         # TODO: maybe incipit title?
         LOD_G.add((res, SKOS.prefLabel, Literal(T_TO_CN[T], lang="zh-hant")))
