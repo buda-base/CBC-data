f4f93e9ddfcd71a014da18bd7c4d754a342c14a4
eroux <elie.roux@telecom-bretagne.eu>
2021-12-09 16:20:59 +0100

    generate outline as derived file

--- derive.py
+++ derive.py
@@ -1,5 +1,32 @@
 import csv
 import json
+import rdflib
+import sys
+from rdflib import URIRef, Literal, BNode
+from rdflib.namespace import RDF, SKOS, RDFS, OWL, Namespace, NamespaceManager, XSD
+
+BF = Namespace("http://id.loc.gov/ontologies/bibframe/")
+BDR = Namespace("http://purl.bdrc.io/resource/")
+BDO = Namespace("http://purl.bdrc.io/ontology/core/")
+TMP = Namespace("http://purl.bdrc.io/ontology/tmp/")
+BDG = Namespace("http://purl.bdrc.io/graph/")
+BDA = Namespace("http://purl.bdrc.io/admindata/")
+ADM = Namespace("http://purl.bdrc.io/ontology/admin/")
+MBBT = Namespace("http://mbingenheimer.net/tools/bibls/")
+CBCT_URI = "https://dazangthings.nz/cbc/text/"
+CBCT = Namespace(CBCT_URI)
+
+NSM = NamespaceManager(rdflib.Graph())
+NSM.bind("bdr", BDR)
+NSM.bind("", BDO)
+NSM.bind("bdg", BDG)
+NSM.bind("bda", BDA)
+NSM.bind("adm", ADM)
+NSM.bind("skos", SKOS)
+NSM.bind("rdf", RDF)
+NSM.bind("cbct", CBCT)
+NSM.bind("mbbt", MBBT)
+NSM.bind("bf", BF)
 
 K_TO_T = {}
 K_TO_SKT = {}
@@ -96,6 +123,60 @@ with open('input/W3CN27014.csv', newline='') as csvfile:
             if t not in T_TO_PART:
                 T_TO_PART[t] = []
             T_TO_PART[t].append("MW3CN27014_"+k)
+
+reg = rdflib.Graph()
+reg.namespace_manager = NSM
+with open('input/W3CN27014.csv', newline='') as csvfile:
+    tkreader = csv.reader(csvfile)
+    next(tkreader)
+    mainparti = 0
+    subparti = 0
+    rootw = BDR["W3CN27014"]
+    rootmw = BDR["MW3CN27014"]
+    previousText = None
+    for row in tkreader:
+        if row[0] == "" or row[3] == "":
+            continue
+        k = normalize_k_id(row[0])
+        mw = BDR["MW3CN27014_"+k]
+        if '-' in k:
+            subparti += 1
+            reg.add((mw, BDO.partOf, previousText))
+            reg.add((mw, BDO.partIndex, Literal(subparti, datatype=XSD.integer)))
+        else:
+            previousText = mw
+            mainparti += 1
+            subparti = 0
+            reg.add((mw, BDO.partOf, rootmw))
+            reg.add((mw, BDO.partIndex, Literal(mainparti, datatype=XSD.integer)))
+        reg.add((mw, RDF.type, BDO.Instance))
+        reg.add((mw, BDO.inRootInstance, rootmw))
+        reg.add((mw, BDO.partType, BDR.PartTypeText))
+        if row[6] != "":
+            t = normalize_taisho_id(row[6])
+            reg.add((mw, BDO.instanceOf, BDR["WA0TTE"+t]))
+        if row[9] != "":
+            reg.add((mw, SKOS.prefLabel, Literal(row[9], lang="zh-hant")))
+            reg.add((mw, SKOS.hiddenLabel, Literal(row[1], lang="zh-hant")))
+        elif row[1] != "":
+            reg.add((mw, SKOS.prefLabel, Literal(row[1], lang="zh-hant")))
+        if row[7] != "":
+            reg.add((mw, SKOS.altLabel, Literal(row[7], lang="ko")))
+        idr = BDR["IDMW3CN27014_"+k]
+        reg.add((mw, BF.identifiedBy, idr))
+        reg.add((idr, RDF.type, BDR.CBCSiglaK))
+        reg.add((idr, RDF.value, Literal(k)))
+        clr = BDR["CLMW3CN27014_"+k]
+        reg.add((mw, BDO.contentLocation, clr))
+        reg.add((clr, RDF.type, BDO.ContentLocation))
+        reg.add((clr, BDO.contentLocationPage, Literal(row[3], datatype=XSD.integer)))
+        reg.add((clr, BDO.contentLocationVolume, Literal(row[2], datatype=XSD.integer)))
+        reg.add((clr, BDO.contentLocationEndPage, Literal(row[4], datatype=XSD.integer)))
+        reg.add((clr, BDO.contentLocationEndVolume, Literal(row[5], datatype=XSD.integer)))
+        reg.add((clr, BDO.contentLocationInstance, rootw))
+
+reg.serialize("derived/MW3CN27014.ttl", format="turtle")
+
 
 with open('derived/t_to_part.json', 'w', encoding='utf-8') as f:
     json.dump(T_TO_PART, f, ensure_ascii=False, indent=4)
